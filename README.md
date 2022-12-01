fea14331de56c96f2851c22b8b644a10f5a04012
BigAchiever <danishali9575@gmail.com>
2022-12-01 22:05:59 +0530

    trial

---
+++ README.md
@@ -0,0 +1,4 @@
+<h2>Fees management system</h2>
+<p>I have made this software to eliminate the scams happining in schools while collecting fees. This software is easy to maintain and small schools can use it to make things easier for their organisation.</p>
+
+
---
+++ manage_student.php
@@ -0,0 +1,79 @@
+<?php 
+include 'db_connect.php'; 
+if(isset($_GET['id'])){
+$qry = $conn->query("SELECT * FROM student where id= ".$_GET['id']);
+foreach($qry->fetch_array() as $k => $val){
+    $$k=$val;
+}
+}
+?>
+<div class="container-fluid">
+    <form action="" id="manage-student">
+        <input type="hidden" name="id" value="<?php echo isset($id) ? $id : '' ?>">
+        <div id="msg" class="form-group"></div>
+        <div class="form-group">
+            <label for="" class="control-label">Admission No.</label>
+            <input type="text" class="form-control" name="id_no"  value="<?php echo isset($id_no) ? $id_no :'' ?>" required>
+        </div>
+        <div class="form-group">
+            <label for="" class="control-label">Class</label>
+            <input type="text" class="form-control" name="class"  value="<?php echo isset($class) ? $class :'' ?>" required>
+        </div>
+        <div class="form-group">
+            <label for="" class="control-label">Student Name</label>
+            <input type="text" class="form-control" name="name"  value="<?php echo isset($name) ? $name :'' ?>" required>
+        </div>
+        <div class="form-group">
+            <label for="" class="control-label">Father Name</label>
+            <input type="text" class="form-control" name="father_name"  value="<?php echo isset($father_name) ? $father_name :'' ?>" required>
+        </div>
+        <div class="form-group">
+            <label for="" class="control-label">Contact</label>
+            <input type="text" class="form-control" name="contact"  value="<?php echo isset($contact) ? $contact :'' ?>" required>
+        </div>
+        <div class="form-group">
+            <label for="" class="control-label">Email ID (Optional)</label>
+            <input type="email" class="form-control" name="email"  value="<?php echo isset($email) ? $email :'' ?>" required>
+        </div>
+        <div class="form-group">
+            <label for="" class="control-label">Address (Optional)</label>
+            <textarea name="address" id="" cols="30" rows="3" class="form-control" required=""><?php echo isset($address) ? $address :'' ?></textarea>
+        </div>
+    </form>
+</div>
+<script>
+    $('#manage-student').on('reset',function(){
+        $('#msg').html('')
+        $('input:hidden').val('')
+    })
+    $('#manage-student').submit(function(e){
+        e.preventDefault()
+        start_load()
+        $('#msg').html('')
+        $.ajax({
+            url:'ajax.php?action=save_student',
+            data: new FormData($(this)[0]),
+            cache: false,
+            contentType: false,
+            processData: false,
+            method: 'POST',
+            type: 'POST',
+            success:function(resp){
+                if(resp==1){
+                    alert_toast("Data successfully saved.",'success')
+                        setTimeout(function(){
+                            location.reload()
+                        },1000)
+                }else if(resp == 2){
+                $('#msg').html('<div class="alert alert-danger mx-2">ID # already exist.</div>')
+                end_load()
+                }   
+            }
+        })
+    })
+
+    $('.select2').select2({
+        placeholder:"Please Select here",
+        width:'100%'
+    })
+</script>
