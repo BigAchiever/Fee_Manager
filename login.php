64ffbee5821ec4df18622b379561932d491586aa
BigAchiever <danishali9575@gmail.com>
2022-12-02 00:06:35 +0530

    Second

--- login.php
+++ login.php
@@ -6,12 +6,12 @@ error_reporting(E_ALL);
 ini_set('display_errors','1');
 include('db_connect.php');
 ob_start();
-// if(!isset($_SESSION['system'])){
+if(!isset($_SESSION['system'])){
 $system = $conn->query("SELECT * FROM system_settings limit 1")->fetch_array();
 foreach ($system as $k => $v) {
 	$_SESSION['system'][$k] = $v;
 }
-// }
+}
 ob_end_flush();
 ?>
 
